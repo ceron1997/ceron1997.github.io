eee0146a9259abead6a57a4ecd05047eeaf77773
ceron1997 <noeceron43@gmail.com>
2024-08-29 07:06:39 -0600

    cambios en el aspecto

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajax({
         url: 'carousel-blog.html',
-        success: function(data) {
+        success: function (data) {
             $('#carouselContainer').html(data);
             $('#carouselBlog').carousel();
         }
@@ -11,26 +11,51 @@ $(document).ready(function() {
 
     $.ajax({
         url: 'carousel-facturacion.html',
-        success: function(data) {
+        success: function (data) {
             $('#carouselContainer1').html(data);
         }
     });
 
-    
+
     $.ajax({
         url: 'carousel-plsql.html',
-        success: function(data) {
+        success: function (data) {
             $('#carouselContainer2').html(data);
         }
     });
 
     $.ajax({
         url: 'carousel-javaee8_pedidos.html',
-        success: function(data) {
+        success: function (data) {
             $('#carousel-javaee8_pedidos').html(data);
         }
     });
 
-    
+    $.ajax({
+        url: 'carousel-rh.html',
+        success: function (data) {
+            $('#carousel-rh').html(data);
+        }
+    });
+
+
+    $.ajax({
+        url: 'carousel-spring.html',
+        success: function (data) {
+            $('#carousel-spring').html(data);
+        }
+    });
+
+    $.ajax({
+        url: 'carousel-javaee.html',
+        success: function (data) {
+            $('#carousel-javaee').html(data);
+        }
+    });
+  
+
+
+
+
 });
 
